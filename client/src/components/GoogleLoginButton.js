c86058febc470bb79eba701cd6f112c670287b82
bong6981 <kbk5190@naver.com>
2022-07-15 22:43:17 +0900

    feat: 구글 로그인 프론트 단에서 토큰 받아오기
    프론트에서 토큰을 받아오는 기능을 구현
    
    issue:#14

--- client/src/components/GoogleLoginButton.js
+++ client/src/components/GoogleLoginButton.js
@@ -1,16 +1,39 @@
 import { GoogleLogin } from '@react-oauth/google';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import axios from 'axios';
 
 const GoogleLoginButton = () => {
+  const clientId = '66570255272-3bdt65gedmn1fia6qi9getkts2fdf6oh.apps.googleusercontent.com'
+  
+  async function onSuccess(res) {
+    // const profile = res.getBasicProfile();
+    // const userdata = {
+    //   email: profile.getEmail(),
+    //   image: profile.getImageUrl(),
+    //   name: profile.getName(),
+    // }; 
+    // 로그인 성공 후 실행하기 원하는 코드 작성.
+    alert("구글 로그인에 성공하였습니다");
+
+    axios({
+      method: "POST",
+      url: "http://localhost:5000/api/googlelogin",
+      data: {tokendId : res.credential}
+    }).then(response => {
+      console.log(response);
+    })
+  }
+
+  const onError = (res) => {
+    alert("구글 로그인에 실패하였습니다");
+    console.log("err", res);
+  };
+
   return (
-    <GoogleOAuthProvider clientId='66570255272-3bdt65gedmn1fia6qi9getkts2fdf6oh.apps.googleusercontent.com'>
+    <GoogleOAuthProvider clientId={clientId}>
       <GoogleLogin
-        onSuccess={(credentialResponse) => {
-          console.log(credentialResponse);
-        }}
-        onError={() => {
-          console.log('Login Failed');
-        }}
+        onSuccess={onSuccess}
+        onError={onError}
       />
     </GoogleOAuthProvider>
   );
