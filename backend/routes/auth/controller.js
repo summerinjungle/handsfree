4f996b4340f78069e52bc2aa632bdbee02f39e6e
bongf <kbk5190@naver.com>
2022-07-16 22:11:33 +0900

    feat: Upsert 구현
    유저가 이미 가입된 유저인지 확인하고 있으면 해당 값만 업데이트
    해주고(name이 바뀌었을 수 있으니) 없다면 새로운 유저를 생성하게 해서
    중복된 유저가 db에 저장되지 않게 한다
    
    issue:#13

--- backend/routes/auth/controller.js
+++ backend/routes/auth/controller.js
@@ -1,8 +1,10 @@
+require("dotenv").config()
 const { OK, CREATED, BAD_REQUEST } = require('../../config/statusCode').statusCode;
 const {OAuth2Client, UserRefreshClient} = require('google-auth-library');
 const client = new OAuth2Client(process.env.NODE_APP_GOOGLE_LOGIN_CLIENT_ID);
 const userServices = require('../../services/user');
 const jwt = require('jsonwebtoken');
+const User = require("../../models/User");
 
 
 /*
@@ -18,12 +20,34 @@ exports.googleLogin = async (req, res, next) => {
             audience: process.env.NODE_APP_GOOGLE_LOGIN_CLIENT_ID
         });
         const { name, email} = ticket.getPayload();
-        const user = await userServices.findByEmail(email);
-        if(user) {
-            const token = jwt.sign({_id:user.email})
-        } else {
+        const login_user = await userServices.upsert({
+            filter: {email: email},
+            update: {
+                name: name,
+                email: email
+            }
+        })
 
-        }
+        res.status(OK).json(login_user);
+
+        
+        
+        // const user = await userServices.findByEmail(email);
+
+
+        // if(user) {
+        //     const token = jwt.sign({data:user.email}, process.env.NODE_APP_JWT_SECRET, {expiresIn: '10m'});
+        //     const {_id, name, email} = user;
+        //     console.log(_id, name, email);
+        //     res.json({
+        //         token,
+        //         user: {_id, name, email}
+        //     })
+        // } else {
+        //     let newUser = new User(name, email);
+
+
+        // }
 
 
     } catch (error) {
