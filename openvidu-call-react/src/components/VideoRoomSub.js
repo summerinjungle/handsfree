48a08955ebd4e3bfbcf8099275e0c58f275478e4
onxmoreplz <hamdoyoung95@gmail.com>
2022-07-16 17:05:12 +0900

    fix: Subscriber 관련 foreach문 추가

--- openvidu-call-react/src/components/VideoRoomSub.js
+++ openvidu-call-react/src/components/VideoRoomSub.js
@@ -311,16 +311,13 @@ class VideoRoomSub extends Component {
     //방장이(Publisher) 나갈때만 호출되어야 함 아직 그건 안됨.
     this.stopRecording(this.state.mySessionId);
     
-    // 방장(Publisher)일 경우에는 모든 Subscriber 강제 종료
-    // let remoteUsers = this.state.subscribers;
-    //   remoteUsers.forEach((user) => {
-    //     if (user.getConnectionId() === event.from.connectionId) {
-    //       this.leaveSession();
-    //     }
-    //   });
-
     console.log("FORCE_DISCONNECT",this.state.mySessionId, this.state.subscribers.connectionId, this.state, this.state.localUser);
-    this.forceDisconnect(this.state.mySessionId, this.state.subscribers.connectionId);
+
+    // 방장(Publisher)일 경우에는 모든 Subscriber 강제 종료
+    let remoteUsers = this.state.subscribers;
+      remoteUsers.forEach((user) => {
+        this.forceDisconnect(this.state.mySessionId, user.getConnectionId());  
+      });
 
   }
 
