efc8aba2685dfd6bcfecff915239dae6bdedde32
Piousangel <yyh7654@gmail.com>
2022-07-17 15:55:00 +0900

    feat : current JsonWebTokenError

--- backend/routes/rooms/index.js
+++ backend/routes/rooms/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const controller = require('./controller');
 
-router.get('/', controller.createRoom);
+router.post('/', controller.createRoom);
 // router.post('/', controller.setRoom);
 
 module.exports = router;
