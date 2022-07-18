ba7f32ac5994207c97b8c11b405032d178a6acd0
Piousangel <yyh7654@gmail.com>
2022-07-18 17:03:05 +0900

    feat : getTime()

--- backend/routes/rooms/controller.js
+++ backend/routes/rooms/controller.js
@@ -12,7 +12,9 @@ var today = new Date();
 var hours = ('0' + today.getHours()).slice(-2); 
 var minutes = ('0' + today.getMinutes()).slice(-2);
 var seconds = ('0' + today.getSeconds()).slice(-2); 
-var timeString = hours + ':' + minutes  + ':' + seconds;
+
+
+var timeString = today.getTime();
 
 //방 생성 API
 exports.createRoom = async (req, res, next) => {
