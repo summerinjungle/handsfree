af01b887724c8f4e3a78ed89c7798e243f6ad453
gunny6026 <gunny6026@naver.com>
2022-07-23 20:40:26 +0900

    feat : 채팅 메세지에 id 값 지정
    편집실에서 막둥아 별표 부분을 음성 기록 부분에 표시하기 위해 채팅
    메시지에 고유 id 값을 부여하였습니다.

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,75 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const connect = require('./database/connection');
-const cookieParser = require('cookie-parser');
-
-const apiRouter = require('./routes');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const connect = require("./database/connection");
+const cookieParser = require("cookie-parser");
+const apiRouter = require("./routes");
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const socket = require("socket.io");
+
 const port = 5000;
+
+const io = socket(server);
+
+const users = {};
+
+const socketToRoom = {};
+
 connect();
 
 //application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 //application/json
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
+});
+
+io.on("connection", (socket) => {
+  console.log("socket  =", socket);
+  socket.on("join room", (roomID) => {
+    console.log("session ID ", roomID);
+    if (users[roomID]) {
+      // const length = users[roomID].length;
+      // if (length === 4) {
+      //   socket.emit("room full");
+      //   return;
+      // }
+      users[roomID].push(socket.id);
+    } else {
+      users[roomID] = [socket.id];
+    }
+    socketToRoom[socket.id] = roomID;
+    const usersInThisRoom = users[roomID].filter((id) => id !== socket.id);
+
+    socket.emit("all users", usersInThisRoom);
+  });
+
+  socket.on("sending signal", (payload) => {
+    io.to(payload.userToSignal).emit("user joined", {
+      signal: payload.signal,
+      callerID: payload.callerID,
+    });
+  });
+
+  socket.on("returning signal", (payload) => {
+    io.to(payload.callerID).emit("receiving returned signal", {
+      signal: payload.signal,
+      id: socket.id,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    const roomID = socketToRoom[socket.id];
+    let room = users[roomID];
+    if (room) {
+      room = room.filter((id) => id !== socket.id);
+      users[roomID] = room;
+    }
+  });
 });
--- openvidu-call-react/src/components/edit/EditingRoom.jsx
+++ openvidu-call-react/src/components/edit/EditingRoom.jsx
@@ -16,199 +16,216 @@ import MarkersPlugin from "wavesurfer.js/dist/plugin/wavesurfer.markers.min.js";
 import { connect } from "react-redux";
 import TextEditor from "./TextEditor";
 import saveButton from "./docx";
+import Voice from "./Voice";
 
 const EditingRoom = ({ props, recordFile, sessionId }) => {
-    const wavesurfer = useRef(null);
-    const [isPlay, setIsPlay] = useState(false);
-    const [volume, setVolume] = useState(1);
-    const [timeWaveSurfer, setTimeWaveSurfer] = useState(null);
+  const wavesurfer = useRef(null);
+  const [isPlay, setIsPlay] = useState(false);
+  const [volume, setVolume] = useState(1);
+  const [timeWaveSurfer, setTimeWaveSurfer] = useState(null);
 
-    const playButton = () => {
-        wavesurfer.current.playPause();
-        if (wavesurfer.current.isPlaying()) {
-            setIsPlay(true);
-        } else {
-            setIsPlay(false);
-        }
-    };
-    const stopButton = () => {
-        wavesurfer.current.stop();
-        setIsPlay(false);
-    };
-
-    const changeVolume = (event) => {
-        setVolume(event.target.valueAsNumber);
-        wavesurfer.current.setVolume(volume);
-    };
-
-    const [chatList, setChatList] = useState([]); // 음성 기록들
-
-    useEffect(() => {
-        console.log("sessionId 입니다", sessionId);
-        loadAllRecord(); // 회의에서 저장된 기록들 가져오기
-
-        wavesurfer.current = WaveSurfer.create({
-            container: ".audio",
-            waveColor: "#FFFFFF",
-            progressColor: "red",
-            barWidth: 3,
-            plugins: [
-                RegionsPlugin.create({}),
-                MarkersPlugin.create({}),
-                CursorPlugin.create({
-                    showTime: true,
-                    opacity: 1,
-                    customShowTimeStyle: {
-                        "background-color": "red",
-                        color: "#fff",
-                        padding: "6px",
-                        "font-size": "15px",
-                    },
-                }),
-            ],
-        });
-    }, []);
-
-    useEffect(() => {
-        if (wavesurfer) {
-            console.log("WaveSurfer 녹음 파일 =====> ", mapStateToProps);
-            //   wavesurfer.current.load(recordFile.url);
-            //   wavesurfer.current.load(testMp3File)
-            wavesurfer.current.load("https://openvidu.shop/openvidu/recordings/"+ sessionId +"/ownweapon.webm") // OPEN_VIDU 주소 전달해주면 됨
-        }
-    }, []);
-
-    useEffect(() => {
-        console.log(parseFloat(timeWaveSurfer) / 1000);
-        wavesurfer.current.play(parseFloat(timeWaveSurfer) / 1000 - 6);
-    }, [timeWaveSurfer]);
-
-    /**
-     * [GET] http://{BASE_URL}/api/rooms/{roomId}/editingroom
-     *
-     * 잡담구간, 별표표시, 음성기록에 필요한 정보들 받아와
-     * WaveSurfer에 뿌려줌
-     *
-     * TODO: 아래 for 반복문 2개 함수로 분리
-     */
-    async function loadAllRecord() {
-        await axios
-            .get("/api/rooms/" + sessionId + "/editingroom") // this.state.roomId 맞나요?
-            .then(function (response) {
-                const { chatList, starList, recordMuteList } = response.data.editingRoom;
-                setChatList(chatList);
-                console.log("WWWWW", response.data.editingRoom);
-                
-                // [잡담 구간] 표시
-                console.log("RecordMuteList", recordMuteList);
-                for (let i = 0; i < recordMuteList.length; i++) {
-                    console.log("left, right!!!!!!",parseFloat(recordMuteList[i].left)/1000);
-                    console.log("left, right!!!!!!",parseFloat(recordMuteList[i].right)/1000);
-                    wavesurfer.current.regions.add({
-                        start: parseFloat(recordMuteList[i].left)/1000 - 6,
-                        end: parseFloat(recordMuteList[i].right)/1000 - 6,
-                        color: "#33CEBFAC",
-                    });
-                }
-
-                // [막둥아 별표] 표시
-                console.log("starList", starList);
-                for (let i = 0; i < starList.length; i++) {
-                    wavesurfer.current.addMarker({
-                        time: parseFloat(starList[i].startTime) / 1000 - 6,
-                        label: "V1",
-                        color: "#FF7715",
-                        position: "top",
-                    });
-                }
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+  const playButton = () => {
+    wavesurfer.current.playPause();
+    if (wavesurfer.current.isPlaying()) {
+      setIsPlay(true);
+    } else {
+      setIsPlay(false);
     }
+  };
+  const stopButton = () => {
+    wavesurfer.current.stop();
+    setIsPlay(false);
+  };
 
-    function getHTMLtoString() {
-      let ns = new XMLSerializer();
-      let korean = `<meta charset="utf-8" />`
-      let targetString = ns.serializeToString(document.querySelector(".ql-editor"));
-      return korean + targetString
+  const changeVolume = (event) => {
+    setVolume(event.target.valueAsNumber);
+    wavesurfer.current.setVolume(volume);
+  };
+
+  const [chatList, setChatList] = useState([]); // 음성 기록들
+
+  useEffect(() => {
+    console.log("sessionId 입니다", sessionId);
+    loadAllRecord(); // 회의에서 저장된 기록들 가져오기
+
+    wavesurfer.current = WaveSurfer.create({
+      container: ".audio",
+      waveColor: "#FFFFFF",
+      progressColor: "red",
+      barWidth: 3,
+      plugins: [
+        RegionsPlugin.create({}),
+        MarkersPlugin.create({}),
+        CursorPlugin.create({
+          showTime: true,
+          opacity: 1,
+          customShowTimeStyle: {
+            "background-color": "red",
+            color: "#fff",
+            padding: "6px",
+            "font-size": "15px",
+          },
+        }),
+      ],
+    });
+  }, []);
+
+  useEffect(() => {
+    if (wavesurfer) {
+      console.log("WaveSurfer 녹음 파일 =====> ", mapStateToProps);
+      //   wavesurfer.current.load(recordFile.url);
+      //   wavesurfer.current.load(testMp3File)
+      wavesurfer.current.load(
+        "https://openvidu.shop/openvidu/recordings/" +
+          sessionId +
+          "/ownweapon.webm"
+      ); // OPEN_VIDU 주소 전달해주면 됨
     }
+  }, []);
 
-    return (
-        <div id='editingroom-container'>
-            <div className='header'>
-                <div className='header-contents'>
-                    <img className='header-logo' src={mainLogo} />
-                </div>
-                <div className='header-contents text-right'>
-                    <button onClick={ () => saveButton(getHTMLtoString()) }>메모 다운로드</button>
-                    <button>나가기</button>
-                </div>
-            </div>
-            <hr className='my-0'></hr>
-            <div className='contents'>
-                <div className='contents-left'>
-                    <div className='contents-label'>메모</div>
-                    <TextEditor sessionId={sessionId}/>
-                </div>
-                <div className='contents-right'>
-                    <div className='contents-label'>음성 기록</div>
-                    <div className='recorditems'>
-                        {chatList &&
-                            chatList.map((recordItem) => (
-                                <ChatItem
-                                    key={recordItem.id}
-                                    userName={recordItem.nickname}
-                                    time = {recordItem.time}
-                                    startTime={recordItem.startTime}
-                                    message={recordItem.message}
-                                    setTimeWaveSurfer={setTimeWaveSurfer}
-                                />
-                            ))}
-                    </div>
-                </div>
-            </div>
-            <div className='audio-container'>
-                {/* <div className='track-name'>The name of the track</div> */}
-                <div className='audio'></div>
-                <div className='buttons'>
-                    <span
-                        className={"play-btn btn" + (isPlay === true ? " playing" : "")}
-                        onClick={playButton}
-                    >
-                        <PlayArrowIcon className='fas fa-play' />
-                        <PauseIcon className='fas fa-pause' />
-                    </span>
+  useEffect(() => {
+    console.log(parseFloat(timeWaveSurfer) / 1000);
+    wavesurfer.current.play(parseFloat(timeWaveSurfer) / 1000 - 6);
+  }, [timeWaveSurfer]);
 
-                    <span className='stop-btn btn' onClick={stopButton}>
-                        <Stop className='fas fa-stop' />
-                    </span>
+  /**
+   * [GET] http://{BASE_URL}/api/rooms/{roomId}/editingroom
+   *
+   * 잡담구간, 별표표시, 음성기록에 필요한 정보들 받아와
+   * WaveSurfer에 뿌려줌
+   *
+   * TODO: 아래 for 반복문 2개 함수로 분리
+   */
+  async function loadAllRecord() {
+    await axios
+      .get("/api/rooms/" + sessionId + "/editingroom") // this.state.roomId 맞나요?
+      .then(function (response) {
+        const { chatList, starList, recordMuteList } =
+          response.data.editingRoom;
+        setChatList(chatList);
+        console.log("WWWWW", response.data.editingRoom);
 
-                    <span className={"mute-btn btn" + (!volume ? " muted" : "")}>
-                        <VolumeUp className='fas fa-volume-up' />
-                        <VolumeOff className='fas fa-volume-mute' />
-                    </span>
+        // [잡담 구간] 표시
+        console.log("RecordMuteList", recordMuteList);
+        for (let i = 0; i < recordMuteList.length; i++) {
+          console.log(
+            "left, right!!!!!!",
+            parseFloat(recordMuteList[i].left) / 1000
+          );
+          console.log(
+            "left, right!!!!!!",
+            parseFloat(recordMuteList[i].right) / 1000
+          );
+          wavesurfer.current.regions.add({
+            start: parseFloat(recordMuteList[i].left) / 1000 - 6,
+            end: parseFloat(recordMuteList[i].right) / 1000 - 6,
+            color: "#33CEBFAC",
+          });
+        }
 
-                    <input
-                        type='range'
-                        min={0}
-                        max={20}
-                        step={1}
-                        value={volume}
-                        className='volume-slider'
-                        onChange={changeVolume}
-                        readOnly
-                    />
-                </div>
-            </div>
-        </div>
+        // [막둥아 별표] 표시
+        console.log("starList", starList);
+        for (let i = 0; i < starList.length; i++) {
+          wavesurfer.current.addMarker({
+            time: parseFloat(starList[i].startTime) / 1000 - 6,
+            label: "V1",
+            color: "#FF7715",
+            position: "top",
+          });
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  function getHTMLtoString() {
+    let ns = new XMLSerializer();
+    let korean = `<meta charset="utf-8" />`;
+    let targetString = ns.serializeToString(
+      document.querySelector(".ql-editor")
     );
+    return korean + targetString;
+  }
+
+  return (
+    <div id='editingroom-container'>
+      <Voice sessionId={sessionId} />
+      <div className='header'>
+        <div className='header-contents'>
+          <img className='header-logo' src={mainLogo} />
+        </div>
+        <div className='header-contents text-right'>
+          <button onClick={() => saveButton(getHTMLtoString())}>
+            메모 다운로드
+          </button>
+          <button>나가기</button>
+        </div>
+      </div>
+      <hr className='my-0'></hr>
+      <div className='contents'>
+        <div className='contents-left'>
+          <div className='contents-label'>메모</div>
+          <TextEditor sessionId={sessionId} />
+        </div>
+        <div className='contents-right'>
+          <div className='contents-label'>음성 기록</div>
+          <div className='recorditems'>
+            {chatList &&
+              chatList.map((recordItem) => (
+                <ChatItem
+                  key={recordItem.id}
+                  userName={recordItem.nickname}
+                  time={recordItem.time}
+                  startTime={recordItem.startTime}
+                  message={recordItem.message}
+                  setTimeWaveSurfer={setTimeWaveSurfer}
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+      <div className='audio-container'>
+        {/* <div className='track-name'>The name of the track</div> */}
+        <div className='audio'></div>
+        <div className='buttons'>
+          <span
+            className={"play-btn btn" + (isPlay === true ? " playing" : "")}
+            onClick={playButton}
+          >
+            <PlayArrowIcon className='fas fa-play' />
+            <PauseIcon className='fas fa-pause' />
+          </span>
+
+          <span className='stop-btn btn' onClick={stopButton}>
+            <Stop className='fas fa-stop' />
+          </span>
+
+          <span className={"mute-btn btn" + (!volume ? " muted" : "")}>
+            <VolumeUp className='fas fa-volume-up' />
+            <VolumeOff className='fas fa-volume-mute' />
+          </span>
+
+          <input
+            type='range'
+            min={0}
+            max={20}
+            step={1}
+            value={volume}
+            className='volume-slider'
+            onChange={changeVolume}
+            readOnly
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
-    return {
-        roomId: state.user.sessionId,
-    };
+  return {
+    roomId: state.user.sessionId,
+  };
 };
 
 export default connect(mapStateToProps)(EditingRoom);
