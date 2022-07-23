8cd25e98e66c1583bf5f9284f31984d928e74cbc
onxmoreplz <hamdoyoung95@gmail.com>
2022-07-23 16:34:21 +0900

    feat: 음성기록 Item 메시지 수정 기능 구현

--- openvidu-call-react/src/components/edit/chat/ChatItem.jsx
+++ openvidu-call-react/src/components/edit/chat/ChatItem.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { useRef, useState } from "react";
 import playButtonImg from "../../../assets/images/playButton.png";
 
 const ChatItem = ({ key, userName, time, startTime, message, setTimeWaveSurfer }) => {
 
-    // const sendStartTime = () => {
-    //     //
-    // }
+    const localInput = useRef();
+
+    const [isEdit, setIsEdit] = useState(false); // 수정버튼 스위치 state
+    const [localContent, setLocalContent] = useState(message); // state기본값을 content로 설정하여, 수정눌렀을때 작성했던 내용을 그대로 불러옴
+
+
+    const toggleIsEdit = () => setIsEdit(!isEdit); // toggleIsEdit()이 호출이 되면 setIsEdit()이 되고 !(Not)연산을 통해 isEdit이 true면 false로, false면 true로 바꿔줌
+
+    // 수정완료 눌렀을때 
+    // 해당 id값과, 새로바뀔 컨텐츠인 localContent를 전달
+    const handleEdit = () => {
+        if (localContent.length < 5) {
+            localInput.current.focus();
+            return;
+        }
+
+        toggleIsEdit(); // 수정하고 나면 수정폼은 닫아줌
+    }
 
     return (
         <div key={key} className="relative mb-20">
@@ -23,10 +39,19 @@ const ChatItem = ({ key, userName, time, startTime, message, setTimeWaveSurfer }
                         {time}
                     </div>
                     <div className="inline-block mx-10">
-                        <button onClick={() => setTimeWaveSurfer(startTime)}><img src={playButtonImg} height="15" width="15" /></button>
+                        <button onClick={() => setTimeWaveSurfer(startTime)}><img src={playButtonImg} height="12" width="10" /></button>
                     </div>
                     <div className="inline-block">
-                        <button>수정</button>
+                        { // 수정중인 상태면 ? 수정완료,취소버튼 보이게. 수정중인 상태가 아니면 : 수정, 삭제 버튼 보이게
+                            isEdit
+                                ? (<>
+                                    <button onClick={handleEdit}>수정완료</button>
+                                </>)
+                                : <>
+
+                                    <button onClick={toggleIsEdit}>수정</button>
+                                </>
+                        }
                         <button>삭제</button>
                         <button>메모 추가</button>
                     </div>
@@ -34,7 +59,19 @@ const ChatItem = ({ key, userName, time, startTime, message, setTimeWaveSurfer }
                 </div>
                 <div className="relative">
                     <div className="message">
-                        {message}
+                        { // 수정중인 상태면 ? 수정폼을 보여주고, 수정중인 상태가 아니면 : 작성한 컨텐츠를 보여줌
+                            isEdit
+                                ? (<>
+                                    <textarea
+                                        ref={localInput}
+                                        value={localContent} // 수정내용 기본값 설정
+                                        onChange={(e) => { setLocalContent(e.target.value) }} />
+                                </>)
+                                : <>
+                                    {localContent}
+                                </>
+                        }
+
                     </div>
                 </div>
             </div>
