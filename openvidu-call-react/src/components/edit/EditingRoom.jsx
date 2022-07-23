2fe5e843cabffef9adb8cbe5db59f97370000b44
onxmoreplz <hamdoyoung95@gmail.com>
2022-07-23 21:55:25 +0900

    chore: 코드 lint 및 로그 기록 코드들 삭제

--- openvidu-call-react/src/components/edit/EditingRoom.jsx
+++ openvidu-call-react/src/components/edit/EditingRoom.jsx
@@ -94,13 +94,12 @@ const EditingRoom = ({ props, recordFile, sessionId }) => {
     //     }
     // }
 
-    
+
     /**
      * 음성기록 리스트 내 아이템 삭제 함수
      */
     function deleteChatItem(paramId) {
         setChatList(chatList.filter(chat => chat.id !== paramId));
-        // setChatList(chatList.filter(chat => chat.id !== paramId));
     };
 
     /**
@@ -122,8 +121,8 @@ const EditingRoom = ({ props, recordFile, sessionId }) => {
                 // [잡담 구간] 표시
                 console.log("RecordMuteList", recordMuteList);
                 for (let i = 0; i < recordMuteList.length; i++) {
-                    console.log("left, right!!!!!!", parseFloat(recordMuteList[i].left) / 1000);
-                    console.log("left, right!!!!!!", parseFloat(recordMuteList[i].right) / 1000);
+                    console.log("left!!!!!!", parseFloat(recordMuteList[i].left) / 1000);
+                    console.log("right!!!!!!", parseFloat(recordMuteList[i].right) / 1000);
                     if (recordMuteList[i].left) { // 없을 때 추가 안 해줌(예외 처리)
                         wavesurfer.current.regions.add({
                             start: parseFloat(recordMuteList[i].left) / 1000 - 6,
@@ -156,149 +155,154 @@ const EditingRoom = ({ props, recordFile, sessionId }) => {
         return korean + targetString
     }
 
-  useEffect(() => {
-    console.log(parseFloat(timeWaveSurfer) / 1000);
-    wavesurfer.current.play(parseFloat(timeWaveSurfer) / 1000 - 6);
-  }, [timeWaveSurfer]);
+    useEffect(() => {
+        console.log(parseFloat(timeWaveSurfer) / 1000);
+        wavesurfer.current.play(parseFloat(timeWaveSurfer) / 1000 - 6);
+    }, [timeWaveSurfer]);
 
-  /**
-   * [GET] http://{BASE_URL}/api/rooms/{roomId}/editingroom
-   *
-   * 잡담구간, 별표표시, 음성기록에 필요한 정보들 받아와
-   * WaveSurfer에 뿌려줌
-   *
-   * TODO: 아래 for 반복문 2개 함수로 분리
-   */
-  async function loadAllRecord() {
-    await axios
-      .get("/api/rooms/" + sessionId + "/editingroom") // this.state.roomId 맞나요?
-      .then(function (response) {
-        const { chatList, starList, recordMuteList } =
-          response.data.editingRoom;
-        setChatList(chatList);
-        console.log("WWWWW", response.data.editingRoom);
+    /**
+     * [GET] http://{BASE_URL}/api/rooms/{roomId}/editingroom
+     *
+     * 잡담구간, 별표표시, 음성기록에 필요한 정보들 받아와
+     * WaveSurfer에 뿌려줌
+     *
+     * TODO: 아래 for 반복문 2개 함수로 분리
+     */
+    async function loadAllRecord() {
+        await axios
+            .get("/api/rooms/" + sessionId + "/editingroom") // this.state.roomId 맞나요?
+            .then(function (response) {
+                const { chatList, starList, recordMuteList } =
+                    response.data.editingRoom;
+                setChatList(chatList);
+                console.log("WWWWW", response.data.editingRoom);
+                console.log("@@@", chatList)
+                console.log("@@@@", response.data.editingRoom.chatList)
+                console.log("@@@@@", response.data.editingRoom.chatList[2].id)
 
-        // [잡담 구간] 표시
-        console.log("RecordMuteList", recordMuteList);
-        for (let i = 0; i < recordMuteList.length; i++) {
-          console.log(
-            "left, right!!!!!!",
-            parseFloat(recordMuteList[i].left) / 1000
-          );
-          console.log(
-            "left, right!!!!!!",
-            parseFloat(recordMuteList[i].right) / 1000
-          );
-          wavesurfer.current.regions.add({
-            start: parseFloat(recordMuteList[i].left) / 1000 - 6,
-            end: parseFloat(recordMuteList[i].right) / 1000 - 6,
-            color: "#33CEBFAC",
-          });
-        }
+                // [잡담 구간] 표시
+                console.log("RecordMuteList", recordMuteList);
+                for (let i = 0; i < recordMuteList.length; i++) {
+                    console.log(
+                        "left, right!!!!!!",
+                        parseFloat(recordMuteList[i].left) / 1000
+                    );
+                    console.log(
+                        "left, right!!!!!!",
+                        parseFloat(recordMuteList[i].right) / 1000
+                    );
+                    wavesurfer.current.regions.add({
+                        start: parseFloat(recordMuteList[i].left) / 1000 - 6,
+                        end: parseFloat(recordMuteList[i].right) / 1000 - 6,
+                        color: "#33CEBFAC",
+                    });
+                }
 
-        // [막둥아 별표] 표시
-        console.log("starList", starList);
-        for (let i = 0; i < starList.length; i++) {
-          wavesurfer.current.addMarker({
-            time: parseFloat(starList[i].startTime) / 1000 - 6,
-            label: "V1",
-            color: "#FF7715",
-            position: "top",
-          });
-        }
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
+                // [막둥아 별표] 표시
+                console.log("starList", starList);
+                for (let i = 0; i < starList.length; i++) {
+                    wavesurfer.current.addMarker({
+                        time: parseFloat(starList[i].startTime) / 1000 - 6,
+                        label: "V1",
+                        color: "#FF7715",
+                        position: "top",
+                    });
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
 
-  function getHTMLtoString() {
-    let ns = new XMLSerializer();
-    let korean = `<meta charset="utf-8" />`;
-    let targetString = ns.serializeToString(
-      document.querySelector(".ql-editor")
+    function getHTMLtoString() {
+        let ns = new XMLSerializer();
+        let korean = `<meta charset="utf-8" />`;
+        let targetString = ns.serializeToString(
+            document.querySelector(".ql-editor")
+        );
+        return korean + targetString;
+    }
+
+    return (
+        <div id='editingroom-container'>
+            <Voice sessionId={sessionId} />
+            <div className='header'>
+                <div className='header-contents'>
+                    <img className='header-logo' src={mainLogo} />
+                </div>
+                <div className='header-contents text-right'>
+                    <button onClick={() => saveButton(getHTMLtoString())}>
+                        메모 다운로드
+                    </button>
+                    <button>나가기</button>
+                </div>
+            </div>
+            <hr className='my-0'></hr>
+            <div className='contents'>
+                <div className='contents-left'>
+                    <div className='contents-label'>메모</div>
+                    <TextEditor sessionId={sessionId} />
+                </div>
+                <div className='contents-right'>
+                    <div className='contents-label'>음성 기록</div>
+                    <div className='recorditems'>
+                        {chatList &&
+                            chatList.map((recordItem) => (
+                                <ChatItem
+                                    key={recordItem.id}
+                                    id={recordItem.id}
+                                    userName={recordItem.nickname}
+                                    time={recordItem.time}
+                                    startTime={recordItem.startTime}
+                                    message={recordItem.message}
+                                    setTimeWaveSurfer={setTimeWaveSurfer}
+                                    deleteChatItem={deleteChatItem}
+                                />
+                            ))}
+                    </div>
+                </div>
+            </div>
+            <div className='audio-container'>
+                {/* <div className='track-name'>The name of the track</div> */}
+                <div className='audio'></div>
+                <div className='buttons'>
+                    <span
+                        className={"play-btn btn" + (isPlay === true ? " playing" : "")}
+                        onClick={playButton}
+                    >
+                        <PlayArrowIcon className='fas fa-play' />
+                        <PauseIcon className='fas fa-pause' />
+                    </span>
+
+                    <span className='stop-btn btn' onClick={stopButton}>
+                        <Stop className='fas fa-stop' />
+                    </span>
+
+                    <span className={"mute-btn btn" + (!volume ? " muted" : "")}>
+                        <VolumeUp className='fas fa-volume-up' />
+                        <VolumeOff className='fas fa-volume-mute' />
+                    </span>
+
+                    <input
+                        type='range'
+                        min={0}
+                        max={20}
+                        step={1}
+                        value={volume}
+                        className='volume-slider'
+                        onChange={changeVolume}
+                        readOnly
+                    />
+                </div>
+            </div>
+        </div>
     );
-    return korean + targetString;
-  }
-
-  return (
-    <div id='editingroom-container'>
-      <Voice sessionId={sessionId} />
-      <div className='header'>
-        <div className='header-contents'>
-          <img className='header-logo' src={mainLogo} />
-        </div>
-        <div className='header-contents text-right'>
-          <button onClick={() => saveButton(getHTMLtoString())}>
-            메모 다운로드
-          </button>
-          <button>나가기</button>
-        </div>
-      </div>
-      <hr className='my-0'></hr>
-      <div className='contents'>
-        <div className='contents-left'>
-          <div className='contents-label'>메모</div>
-          <TextEditor sessionId={sessionId} />
-        </div>
-        <div className='contents-right'>
-          <div className='contents-label'>음성 기록</div>
-          <div className='recorditems'>
-            {chatList &&
-              chatList.map((recordItem) => (
-                <ChatItem
-                  key={recordItem.id}
-                  userName={recordItem.nickname}
-                  time={recordItem.time}
-                  startTime={recordItem.startTime}
-                  message={recordItem.message}
-                  setTimeWaveSurfer={setTimeWaveSurfer}
-                />
-              ))}
-          </div>
-        </div>
-      </div>
-      <div className='audio-container'>
-        {/* <div className='track-name'>The name of the track</div> */}
-        <div className='audio'></div>
-        <div className='buttons'>
-          <span
-            className={"play-btn btn" + (isPlay === true ? " playing" : "")}
-            onClick={playButton}
-          >
-            <PlayArrowIcon className='fas fa-play' />
-            <PauseIcon className='fas fa-pause' />
-          </span>
-
-          <span className='stop-btn btn' onClick={stopButton}>
-            <Stop className='fas fa-stop' />
-          </span>
-
-          <span className={"mute-btn btn" + (!volume ? " muted" : "")}>
-            <VolumeUp className='fas fa-volume-up' />
-            <VolumeOff className='fas fa-volume-mute' />
-          </span>
-
-          <input
-            type='range'
-            min={0}
-            max={20}
-            step={1}
-            value={volume}
-            className='volume-slider'
-            onChange={changeVolume}
-            readOnly
-          />
-        </div>
-      </div>
-    </div>
-  );
 };
 
 const mapStateToProps = (state) => {
-  return {
-    roomId: state.user.sessionId,
-  };
+    return {
+        roomId: state.user.sessionId,
+    };
 };
 
 export default connect(mapStateToProps)(EditingRoom);
