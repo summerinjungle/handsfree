49389e48dd75b7fadc88d100cda25fce107e40f9
bongf <kbk5190@naver.com>
2022-07-24 16:54:06 +0900

    feat: 해당 텍스트 내보내기 구현

--- openvidu-call-react/src/components/edit/chat/ChatItem.jsx
+++ openvidu-call-react/src/components/edit/chat/ChatItem.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useRef, useState } from "react";
 import playButtonImg from "../../../assets/images/playButton.png";
 import markerImg from "../../../assets/images/markerImg.png";
+import { insertText } from "../TextEditor"
 
 const ChatItem = ({ key, id, userName, time, startTime, isMarker, message, playTimeWaveSurfer, deleteChatItem }) => {
 
@@ -16,11 +17,6 @@ const ChatItem = ({ key, id, userName, time, startTime, isMarker, message, playT
     // 수정완료 눌렀을때 
     // 해당 id값과, 새로바뀔 컨텐츠인 localContent를 전달
     const handleEdit = () => {
-        if (localContent.length < 5) {
-            localInput.current.focus();
-            return;
-        }
-
         toggleIsEdit(); // 수정하고 나면 수정폼은 닫아줌
     }
 
@@ -28,16 +24,15 @@ const ChatItem = ({ key, id, userName, time, startTime, isMarker, message, playT
         <div key={key} className="relative mb-20">
             <div>
                 <div className="absolute t-40">
-                    { // 수정중인 상태면 ? 수정완료,취소버튼 보이게. 수정중인 상태가 아니면 : 수정, 삭제 버튼 보이게
+                    {
                         isMarker
                             ? (<>
                                 <img src={markerImg} height="12" width="12" />
                             </>)
                             : <>
-                                {/* 아무것도 안 찍힘 */}
+                                {/* 마커 없는 경우 -> 아무것도 안 찍힘 */}
                             </>
                     }
-                    
                 </div>
             </div>
             <div className="pl-20">
@@ -63,7 +58,9 @@ const ChatItem = ({ key, id, userName, time, startTime, isMarker, message, playT
                                 </>
                         }
                         <button onClick={() => deleteChatItem(id)}>삭제</button>
-                        <button>메모 추가</button>
+                        <button onClick={() => insertText('Hello World!!!!')}>메모장에 추가</button>
+                        <div>
+                        </div> 
                     </div>
 
                 </div>
@@ -81,7 +78,6 @@ const ChatItem = ({ key, id, userName, time, startTime, isMarker, message, playT
                                     {localContent}
                                 </>
                         }
-
                     </div>
                 </div>
             </div>
