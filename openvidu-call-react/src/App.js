f8f6338610641ca443ab2137a0c0a1cf19e8ecf3
gunny6026 <gunny6026@naver.com>
2022-07-25 23:46:49 +0900

    chore : 충돌 해결

--- openvidu-call-react/src/App.js
+++ openvidu-call-react/src/App.js
@@ -6,12 +6,7 @@ import EditingRoom from "./components/edit/EditingRoom.jsx";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { getUserNameInCookie } from "./main/cookie";
 import { useSelector, useDispatch } from "react-redux";
-import VoiceRoom from "./components/voiceroom/VoiceRoom";
-import {
-  changeSession,
-  changeIsPublisher,
-  changeUserName,
-} from "./store.js";
+import { changeSession, changeIsPublisher, changeUserName } from "./store.js";
 
 const App = () => {
   const navigate = useNavigate();
@@ -49,7 +44,6 @@ const App = () => {
           element={<EditingRoom sessionId={sessionId} />}
         ></Route>
         <Route path={"/*"} element={<div> 없는페이지 입니다. </div>} />
-        <Route path={"/voice"} element={<VoiceRoom />} />
       </Routes>
     </div>
   );
--- openvidu-call-react/src/components/edit/EditingRoom.jsx
+++ openvidu-call-react/src/components/edit/EditingRoom.jsx
@@ -16,18 +16,13 @@ import MarkersPlugin from "wavesurfer.js/dist/plugin/wavesurfer.markers.min.js";
 import { connect, useSelector } from "react-redux";
 import TextEditor from "./TextEditor";
 import saveButton from "./docx";
-<<<<<<< HEAD
-=======
-import Voice from "../voiceroom/Voice";
-import VoiceRoom from "../voiceroom/VoiceRoom";
->>>>>>> c3e36d38f1637cc5fc47a24d0d8b59dd558b89e0
 import { useNavigate } from "react-router-dom";
 
 const EditingRoom = ({ sessionId }) => {
   let reduxCheck = useSelector((state) => {
     return state;
   });
-  let newSessionId = 'edit' + sessionId;
+  let newSessionId = "edit" + sessionId;
   // let gap = parseFloat(localStorage.getItem("createAt") - reduxCheck.user.createdAt) / 1000 -1;
   const sessionStartTime = parseFloat(localStorage.getItem("createAt")) + 1400;
   console.log(localStorage.getItem("createAt"));
@@ -89,7 +84,7 @@ const EditingRoom = ({ sessionId }) => {
     if (wavesurfer) {
       console.log("WaveSurfer 녹음 파일 =====> ", mapStateToProps);
       //   wavesurfer.current.load(recordFile.url);
-        // wavesurfer.current.load(testMp3File)
+      // wavesurfer.current.load(testMp3File)
       wavesurfer.current.load(
         "https://hyunseokmemo.shop/openvidu/recordings/" +
           sessionId +
@@ -97,7 +92,6 @@ const EditingRoom = ({ sessionId }) => {
       ); // OPEN_VIDU 주소 전달해주면 됨
     }
   }, []);
-
 
   /**
    * [GET] http://{BASE_URL}/api/rooms/{roomId}/editingroom
@@ -119,8 +113,14 @@ const EditingRoom = ({ sessionId }) => {
         // [잡담 구간] 표시
         console.log("RecordMuteList", recordMuteList);
         for (let i = 0; i < recordMuteList.length; i++) {
-          console.log("left!!!!!!", (recordMuteList[i].left - sessionStartTime) / 1000);
-          console.log("right!!!!!!", (recordMuteList[i].right - sessionStartTime) / 1000);
+          console.log(
+            "left!!!!!!",
+            (recordMuteList[i].left - sessionStartTime) / 1000
+          );
+          console.log(
+            "right!!!!!!",
+            (recordMuteList[i].right - sessionStartTime) / 1000
+          );
 
           wavesurfer.current.regions.add({
             start: parseFloat(recordMuteList[i].left - sessionStartTime) / 1000,
@@ -211,11 +211,13 @@ const EditingRoom = ({ sessionId }) => {
           >
             음성기록 다운로드
           </button>
-          <button className='exit' onClick={() => {
-            navigate("/");
-            window.location.reload();
-          }
-            }>
+          <button
+            className='exit'
+            onClick={() => {
+              navigate("/");
+              window.location.reload();
+            }}
+          >
             나가기
           </button>
         </div>
@@ -279,7 +281,7 @@ const EditingRoom = ({ sessionId }) => {
           />
         </div>
       </div>
-      <VoiceRoom sessionId={newSessionId}/>
+      {/* <VoiceRoom sessionId={newSessionId} /> */}
     </div>
   );
 };
