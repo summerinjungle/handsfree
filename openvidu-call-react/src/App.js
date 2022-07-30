c0886a8abea1cc1bb7a66346939ebd730140dfea
minsung37 <tampang99@naver.com>
2022-07-31 04:44:41 +0900

    feat : 비로그인 url 접근 방지
    로그인 없이 url로 들어오는 경우 막았습니다.

--- openvidu-call-react/src/App.js
+++ openvidu-call-react/src/App.js
@@ -4,7 +4,7 @@ import VideoRoomHandsFree from "./components/videoroom/VideoRoomHandsFree";
 import Main from "./main/main";
 import EditingRoom from "./components/edit/EditingRoom.jsx";
 import { Routes, Route, useNavigate } from "react-router-dom";
-import { getUserNameInCookie } from "./main/cookie";
+import { getUserNameInCookie, getTokenInCookie } from "./main/cookie";
 import { useSelector, useDispatch } from "react-redux";
 import { changeSession, changeIsPublisher, changeUserName } from "./store.js";
 import Anon from "./main/anon";
@@ -26,25 +26,33 @@ const App = () => {
   }
   let meetingPath = "/meeting/" + sessionId;
   let editPath = meetingPath + "/edit";
-
+  let isLogin = getTokenInCookie();
   return (
     <div className='App'>
       <Routes>
+        {
+          isLogin === undefined ? (
+            <Route path='/*' element={<Main />} />
+          ):(
+            <>
+            <Route
+              path='/meeting'
+              element={<VideoRoomHandsFree user={user} navigate={navigate} />}
+            >
+              <Route
+                path={sessionId}
+                element={<VideoRoomHandsFree user={user} navigate={navigate} />}
+              />
+            </Route>
+            <Route
+              path={editPath}
+              element={<EditingRoom sessionId={sessionId} />}
+            ></Route>
+            <Route path="/*" element={<Anon />} />
+            </>
+          )
+        }
         <Route path='/' element={<Main />} />
-        <Route
-          path='/meeting'
-          element={<VideoRoomHandsFree user={user} navigate={navigate} />}
-        >
-          <Route
-            path={sessionId}
-            element={<VideoRoomHandsFree user={user} navigate={navigate} />}
-          />
-        </Route>
-        <Route
-          path={editPath}
-          element={<EditingRoom sessionId={sessionId} />}
-        ></Route>
-        <Route path={"/*"} element={<Anon />} />
       </Routes>
     </div>
   );
